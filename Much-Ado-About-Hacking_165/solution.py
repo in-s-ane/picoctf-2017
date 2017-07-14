da0baecd521b43ff8d9afbf61f912a791abee2de
James Wang <jameswang9909@hotmail.com>
2017-07-14 17:43:30 -0400

    Add Much Ado About Hacking

--- Much-Ado-About-Hacking_165/solution.py
+++ Much-Ado-About-Hacking_165/solution.py
@@ -25,3 +25,13 @@ while len(correct) < len(desired):
         break
 
 print correct
+
+"""
+After spending much time trying to get the spl to compile the given program, we can finally examine its
+behavior. The behavior is deterministic, meaning the same input will produce the same output. Because of this, we
+can brute force the correct input we need to generate the desired output.
+
+Alternatively, its possible to decode the program by reading the documentation of the language.
+
+Its@MidSuMm3rNights3xpl0!t
+"""
